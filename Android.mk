fa3c1acfe17241f756f2aa085fe4fba4f9ad26df
Elliot Stirling <e.stirling@xreach.co.uk>
2013-07-17 13:07:32 +0100

    Renamed package Firewall

--- Android.mk
+++ Android.mk
@@ -5,7 +5,7 @@ LOCAL_MODULE_TAGS := optional
 
 LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
 LOCAL_SDK_VERSION := current
-LOCAL_PACKAGE_NAME := droidwall
+LOCAL_PACKAGE_NAME := Firewall
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)

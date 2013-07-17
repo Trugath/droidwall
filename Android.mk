91000648c9fb34d9f63bcb0fa8d288f2f7306424
Elliot Stirling <e.stirling@xreach.co.uk>
2013-07-17 13:37:28 +0100

    More changed to make it play nice with AOSP

--- Android.mk
+++ Android.mk
@@ -1,11 +1,9 @@
 LOCAL_PATH := $(call my-dir)
 include $(CLEAR_VARS)
 
-LOCAL_MODULE_TAGS := optional
-
-LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
-LOCAL_SDK_VERSION := current
+LOCAL_STATIC_JAVA_LIBRARIES := android-support-v4
 LOCAL_PACKAGE_NAME := Firewall
+LOCAL_SRC_FILES := $(call all-java-files-under, src) $(call all-renderscript-files-under, src)
 LOCAL_CERTIFICATE := platform
 
 include $(BUILD_PACKAGE)
--- src/com/googlecode/droidwall/MainActivity.java
+++ src/com/googlecode/droidwall/MainActivity.java
@@ -23,7 +23,6 @@
 
 package com.googlecode.droidwall;
 
-import android.app.Activity;
 import android.app.AlertDialog;
 import android.app.ProgressDialog;
 import android.content.DialogInterface;
@@ -36,6 +35,7 @@ import android.os.AsyncTask;
 import android.os.Bundle;
 import android.os.Handler;
 import android.os.Message;
+import android.support.v4.app.FragmentActivity;
 import android.util.Log;
 import android.view.KeyEvent;
 import android.view.LayoutInflater;
@@ -64,9 +64,9 @@ import java.util.Comparator;
  * Main application activity.
  * This is the screen displayed when you open the application
  */
-public class MainActivity extends Activity implements OnCheckedChangeListener, OnClickListener {
-	
-	// Menu options
+public class MainActivity extends FragmentActivity implements OnCheckedChangeListener, OnClickListener {
+
+    // Menu options
 	private static final int MENU_DISABLE	= 0;
 	private static final int MENU_TOGGLELOG	= 1;
 	private static final int MENU_APPLY		= 2;
